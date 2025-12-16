91ce585580d4aa82e63f53833be5bf288a70ae4e
M.S Swaroop <swaroopms658@gmail.com>
2025-12-16 18:55:12 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+require("dotenv").config(); // <-- line 4
 
 const app = express();
 app.use(cors());
@@ -12,7 +12,7 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.error(err));
 
-app.use("/tasks", require("./routes/taskRoutes"));
+app.use("/tasks", require("./routes/taskRoutes")); // <-- usually line ~15
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
